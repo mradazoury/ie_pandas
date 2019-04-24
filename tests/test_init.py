d5ea2ef69efc701bffc27dff437847e8beb35b0b
rouzbehrahimi <roozbehr.rahimi@gmail.com>
2019-04-24 19:05:46 +0200

    final edition

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,15 +1,16 @@
 import pytest
-from ie_pandas import DataFrame
+import numpy as np
+from ie_pandas.DataFrame import DataFrame
 
 
-def input_type_test():
+def test_input_type():
     """This test checks to make sure only dictionaries are allowed"""
     input_list = [1, 2, 3]
     with pytest.raises(ValueError):
         assert DataFrame(input_list)
 
 
-def input_value_test():
+def test_input_value():
     """ This test checks to make sure that values other than lists and 
     numpy arrays are not allowed"""
     input_dic = {"a": 2, "b": 3}
@@ -17,14 +18,14 @@ def input_value_test():
         assert DataFrame(input_dic)
 
 
-def input_length_test():
+def test_input_length():
     """This test checks to make sure columns are of the same length"""
     input_dic = {"a": [1, 2, 3], "b": [3, 4, 5, 6]}
     with pytest.raises(ValueError):
         assert DataFrame(input_dic)
 
 
-def input_same_value_test():
+def test_input_same_value():
     """This test checks to make sure values in each columns are of the same type"""
     input_dic = {"a": [1, 2, 3, "g"], "b": [3, 4, 5, 6]}
     with pytest.raises(ValueError):
